d11c0bc4d92d009fcf9fb2e7a028c051ab489173
s2u9a <muse83831@hotmail.com>
2015-10-27 18:49:15 -0700

    fixed the anonymous variables and removed =:=

--- project1_p1.pl
+++ project1_p1.pl
@@ -3,7 +3,7 @@
 :- consult('wn_g.pl').
 
 
-definition(S, G) :- s(_ids,_,S,_,_,_), g(_idg, G), _ids =:= _idg,!.
+definition(S, G) :- s(_id,_,S,_,_,_), g(_id, G).
 
 
 /*
@@ -11,9 +11,19 @@ Output:
 ?- definition('hello', G).
 G = 'an expression of greeting; "every morning they exchanged polite hellos"'.
 
-?- definition('discipula',G).
+?- definition('discipula', G).
 false.
 
-?- definition('hypernym',G).
+?- definition('hypernym', G).
 G = 'a word that is more generic than a given word'.
+
+?- definition(S, 'a plant or animal that is atypically small').
+S = dwarf ;
+false.
+
+?- definition('nutrient', G).
+G = 'any substance that can be metabolized by an animal to give energy and build tissue' ;
+G = 'any substance (such as a chemical element or inorganic compound) that can be taken in by a green plant and used in organic synthesis' ;
+G = 'of or providing nourishment; "good nourishing stew"'.
+
 */
